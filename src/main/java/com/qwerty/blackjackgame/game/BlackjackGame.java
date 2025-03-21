3d8f604b5c9fa186ee013961ad610984f13d31fc
Mironov_Roman <psychokid534@gmail.com>
2025-03-21 07:14:45 +0300

    fix surender

--- src/main/java/com/qwerty/blackjackgame/game/BlackjackGame.java
+++ src/main/java/com/qwerty/blackjackgame/game/BlackjackGame.java
@@ -40,7 +40,8 @@ public class BlackjackGame {
         if (bet > playerChips || bet <= 0) {
             throw new IllegalArgumentException("Неверная сумма ставки");
         }
-
+        
+        surrendered = false;
         playerBet = bet;
         playerHand.clear();
         dealerHand.clear();
